ebdd94bc0f2f9c7a04f0bf095fb7710b043a8277
Braku <barukbumer@gmail.com>
2019-10-07 10:17:46 -0500

    .

--- Database/Database/MainWindow.xaml.cs
+++ Database/Database/MainWindow.xaml.cs
@@ -29,9 +29,89 @@ namespace Database
             InitializeComponent();
             con = new OleDbConnection();
             con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\Alumnos.mdb";
+            MostrarDatos();
+        }
+
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from 5B";
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+            if (dt.Rows.Count > 0)
+            {
+                lbcontent.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbcontent.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void LimpiarTodo()
+        {
+            txtid.Text = "";
+            txtname.Text = "";
+            cbGen.SelectedIndex = 0;
+            txttel.Text = "";
+            txtdir.Text = "";
         }
 
         private void bnew_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bedit_click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            if(txtid.Text != "")
+            {
+                if(cbGen.Text != "Selecciona Genero")
+                {
+                    cmd.CommandText = "Insert into 5B(Id, Nombre, Genero, Telefono, Direccion)" + 
+                        "Values("+txtid.Text+", '"+txtname.Text+", '"+cbGen.Text+", "+txttel.Text+", '"+txtdir.Text+"')";
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Alumno agregadon con exito...");
+                    LimpiarTodo();
+                }
+                else
+                {
+                    MessageBox.Show("Favor de seleccionar el genero...");
+                }
+            }
+            else
+            {
+                cmd.CommandText = "update 5B set Nombre ='" +
+                    txtname.Text + "',Genero='" + cbGen.Text + "',Telefono=" + txttel.Text + ",Direccion='" + txtdir.Text + "where Id=" + txtid.Text;
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Datod frl alu,no  actualizados");
+                LimpiarTodo();
+            }
+        }
+
+        private void bdel_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bcancel_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bsal_click(object sender, RoutedEventArgs e)
         {
 
         }
